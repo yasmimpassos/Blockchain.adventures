09844b00e946d3559f7b419c4092f4e59d29b767
yasmimpassos <159191294+yasmimpassos@users.noreply.github.com>
2024-03-06 17:48:05 -0300

    Update Yasmim.sol

--- Yasmim.sol
+++ Yasmim.sol
@@ -1,1 +1,37 @@
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract Yasmim is ERC20, ERC20Burnable, ERC20Pausable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("yasmim", "YSM")
+        Ownable(initialOwner)
+        ERC20Permit("yasmim")
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) private onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal virtual 
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
